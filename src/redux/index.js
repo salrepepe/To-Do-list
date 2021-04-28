862140ed9da5290479c921c0ad2db9382c3d34bc
salrepepe <77669788+salrepepe@users.noreply.github.com>
2021-04-28 16:29:33 +0600

    fix

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,6 +1,6 @@
 import {createStore, compose} from 'redux';
 import rootReducer from "./reducers";
-import {composeWithDevTools} from "redux-devtools-extension/logOnlyInProduction";
+import {composeWithDevTools} from "redux-devtools-extension";
 
 const initState = {};
 const composeFunc = process.env.NODE_ENV === 'development' ? composeWithDevTools() : compose();
